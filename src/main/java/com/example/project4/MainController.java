ffff3ac85149d3236cf3de7877cd6649c01fe0dc
wesamsaleh <wesamssaleh@gmail.com>
2021-11-13 21:27:11 -0500

    UI for customizing pizza

--- src/main/java/com/example/project4/MainController.java
+++ src/main/java/com/example/project4/MainController.java
@@ -1,14 +1,74 @@
 package com.example.project4;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField phoneNumber;
+
+    @FXML
+    void viewCurrentOrder(ActionEvent event) {
+
     }
+
+    @FXML
+    void viewDeluxeOrder(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("pizza-view.fxml"));
+        Parent root = (Parent) loader.load();
+        PizzaController pizzaView = loader.getController();
+        pizzaView.setMainController(this);
+
+        Stage stage = new Stage();
+        Scene scene = new Scene(root, 430, 516);
+        stage.setTitle("Customize your Pizza");
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+    }
+
+    @FXML
+    void viewHawaiianOrder(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("pizza-view.fxml"));
+        Parent root = (Parent) loader.load();
+        PizzaController pizzaView = loader.getController();
+        pizzaView.setMainController(this);
+
+        Stage stage = new Stage();
+        Scene scene = new Scene(root, 430, 516);
+        stage.setTitle("Customize your Pizza");
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+    }
+
+    @FXML
+    void viewPepperoniOrder(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("pizza-view.fxml"));
+        Parent root = (Parent) loader.load();
+        PizzaController pizzaView = loader.getController();
+        pizzaView.setMainController(this);
+
+        Stage stage = new Stage();
+        Scene scene = new Scene(root, 430, 516);
+        stage.setTitle("Customize your Pizza");
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+    }
+
+    @FXML
+    void viewStoreOrders(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/com/example/project4/PizzaController.java
+++ src/main/java/com/example/project4/PizzaController.java
@@ -1,14 +1,25 @@
 package com.example.project4;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class PizzaController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PizzaController{
+
+    private MainController mainController;
+
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void setMainController(MainController controller){
+        this.mainController = controller;
     }
 }
