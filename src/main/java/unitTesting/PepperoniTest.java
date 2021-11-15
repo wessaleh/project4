6d8790cd4ca606f6ca3e0959e16acdc59a58ccdb
Najib Assadullah <najibassdullah@gmail.com>
2021-11-15 01:01:36 -0500

    JUnit Tests for Pepperoni class

---
+++ src/main/java/unitTesting/PepperoniTest.java
@@ -0,0 +1,48 @@
+package unitTesting;
+
+import com.example.project4.Pepperoni;
+import com.example.project4.Size;
+import com.example.project4.Topping;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for Pepperoni class
+ * @author Wesam Saleh, Najibullah Assadullah
+ */
+
+class PepperoniTest {
+    @org.junit.jupiter.api.Test
+    void price() {
+        ArrayList<Topping> topping = new ArrayList<>();
+
+        Pepperoni defaultPizza = new Pepperoni();
+        assertTrue(defaultPizza.price() == 8.99);
+
+        topping.remove(Topping.Pepperoni);
+        Pepperoni pizzaWithoutToppings = new Pepperoni(topping, Size.Small);
+        assertTrue(pizzaWithoutToppings.price() == 8.99);
+        topping.add(Topping.Pepperoni);
+
+
+        topping.add(Topping.Beef);
+        Pepperoni pizzaSmall = new Pepperoni(topping, Size.Small);
+        assertTrue(pizzaSmall.price() == 10.48);
+
+        Pepperoni pizzaMedium = new Pepperoni(topping, Size.Medium);
+        assertTrue(pizzaMedium.price() == 12.48);
+
+        Pepperoni pizzaLarge = new Pepperoni(topping, Size.Large);
+        assertTrue(pizzaLarge.price() == 14.48);
+
+        topping.add(Topping.Cheese);
+        topping.add(Topping.Pineapple);
+        topping.add(Topping.BlackOlives);
+        topping.add(Topping.Chicken);
+        topping.add(Topping.GreenPepper);
+        Pepperoni pizzaMaxToppings = new Pepperoni(topping, Size.Small);
+        assertTrue(pizzaMaxToppings.price() == 17.93);
+    }
+}
