788761a22bbe99a2ab47cc7c12bbc3621cfb4182
wesamsaleh <wesamssaleh@gmail.com>
2021-11-15 18:20:08 -0500

    Making it halal

--- src/main/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.project4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.junit.jupiter.api;
-
-    opens com.example.project4 to javafx.fxml;
-    exports com.example.project4;
-}
